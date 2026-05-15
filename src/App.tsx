9a7604404071945e49b425b1fa3f1e5f0baaddaf
Aleksandrova Olga <bulbenko@yandex.ru>
2026-05-15 13:44:17 +0200

    feat: add base roots

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import Home from './pages/Home';
+import { NavLink } from 'react-router';
+import AppRoutes from './routes/routes';
 const App = () => {
   return (
     <>
-      <Home />
+      <header>
+        {' '}
+        <nav>
+          <NavLink to="/" end>
+            home
+          </NavLink>
+          <NavLink to="/about">about</NavLink>
+        </nav>
+      </header>
+      <AppRoutes />
     </>
   );
 };
---
+++ src/components/CardDetails.tsx
@@ -0,0 +1,11 @@
+import { cardStyles } from '../shared/styles/card-pokemon';
+
+const CardDetails = () => {
+  return (
+    <div className={cardStyles.card}>
+      <p> Detalis</p>
+    </div>
+  );
+};
+
+export default CardDetails;
