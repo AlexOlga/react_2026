96360feeefc6a12847518760efd8fcdef4d653e8
Aleksandrova Olga <bulbenko@yandex.ru>
2026-05-21 20:51:00 +0200

    style: add dark and light theme styles

--- src/shared/styles/page.ts
+++ src/shared/styles/page.ts
@@ -1,6 +1,7 @@
 export const pageStyles = {
   title: 'text-3xl font-bold text-red-400 mb-6',
-  text: 'text-lg leading-8 text-gray-700',
+  text: 'text-lg leading-8 text-gray-700 dark:text-white',
   link: 'text-red-400 font-medium hover:text-red-800 underline transition-colors duration-200 mx-1',
-  container: 'max-w-3xl mx-auto p-8 bg-white flex justify-center flex-col',
+  container:
+    'max-w-3xl mx-auto p-8 bg-white flex justify-center flex-col dark:bg-zinc-900 dark:text-white',
 };
